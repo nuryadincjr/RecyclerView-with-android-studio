8b8690efd442825fb9606b4ad7fb3a78bae6972f
abugrayhat <abugrayhat@gmail.com>
2021-04-01 13:19:28 +0700

    first commit

---
+++ app/src/main/java/com/abuunity/latihanfragmant/MainActivity.java
@@ -0,0 +1,54 @@
+package com.abuunity.latihanfragmant;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationItemView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationItemView = findViewById(R.id.bottom_navigation);
+        bottomNavigationItemView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_home:
+                        getFragmentPage(new HomeFragment());
+                        break;
+                    case R.id.menu_favorite:
+                        getFragmentPage(new FavoritFragment());
+                        break;
+                    case R.id.menu_about:
+                        getFragmentPage(new AboutFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private boolean getFragmentPage(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_container, fragment)
+                    .commit();
+            return true;
+        }
+        return true;
+    }
+}
